0b0ab81927a67544d69bdccf2847edba8631969d
coderbulbul <coderbulbul@gmail.com>
2024-07-06 17:30:18 +0600

    this is first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+// Import Dependencies
+const express = require("express");
+
+const app = express();
+
+// Routing
+app.get("/", (req, res) => {
+  res.send("Get request to homepag");
+});
+
+app.listen(8000, () => {
+  console.log("App is running on port 8000");
+});
